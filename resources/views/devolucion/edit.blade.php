a03239655f0d5f806dbb827189f068cddc14ab5c
Carlos_Sagastume <care_sagastumea@unicah.edu>
2025-04-10 23:23:40 -0600

    BioPharma

--- resources/views/devolucion/edit.blade.php
+++ resources/views/devolucion/edit.blade.php
@@ -17,7 +17,7 @@
                 {{-- Detalle de venta --}}
                 <div class="mb-3">
                     <label for="detalle_venta_id" class="form-label">Detalle de Venta (Venta ID - Medicamento)</label>
-                    <select name="detalle_venta_id" id="detalle_venta_id" class="form-select" required>
+                    <select name="detalle_venta_id" id="detalle_venta_id" class="form-select" required readOnly>
                         <option value="">Seleccione un detalle de venta</option>
                         @foreach ($detalleVentas as $detalle)
                             <option 
