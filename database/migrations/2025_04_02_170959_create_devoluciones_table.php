3971657a9371ee0f65373d72fb8ab47c0176179b
Carlos_Sagastume <care_sagastumea@unicah.edu>
2025-04-10 22:07:12 -0600

    BioPharma

--- database/migrations/2025_04_02_170959_create_devoluciones_table.php
+++ database/migrations/2025_04_02_170959_create_devoluciones_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('devoluciones', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('detalle_venta_id');
-            $table->foreign('detalle_venta_id')->references('id')->on('detalle_ventas')->onDelete('cascade');            
-            $table->foreignId('usuario_id')->constrained('usuarios')->onDelete('cascade');
+            $table->foreign('detalle_venta_id')->references('id')->on('detalle_ventas')->onDelete('cascade');
+            $table->foreignId('usuario_id')->constrained()->onDelete('cascade');
             $table->integer('cantidad');
             $table->date('fecha');
             $table->string('motivo');
